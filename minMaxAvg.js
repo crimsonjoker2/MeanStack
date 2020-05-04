1c99d7a37d1fc8cca7921b2a4a84068b60ff8a63
Daniel Long <skillhaven@outlook.com>
2020-05-04 16:30:10 -0500

    Coin Change

--- minMaxAvg.js
+++ minMaxAvg.js
@@ -87,17 +87,53 @@
 
 //------------------------------
 
-function bubbleSort(arr) {
-    for (i = 0; i < arr.length; i++) {
-        for (y = 0; y < arr.length - i; y++) {
-            if (arr[y] > arr[y + 1]) {
-                temp = arr[y]
-                arr[y] = arr[y + 1]
-                arr[y + 1] = temp
-            }
-        }
+// function bubbleSort(arr) {
+//     for (i = 0; i < arr.length; i++) {
+//         for (y = 0; y < arr.length - i; y++) {
+//             if (arr[y] > arr[y + 1]) {
+//                 temp = arr[y]
+//                 arr[y] = arr[y + 1]
+//                 arr[y + 1] = temp
+//             }
+//         }
+//     }
+//     console.log(arr)
+// }
+
+// bubbleSort([5, 3, 1, 6, 7, 2, 8, 4])
+
+//----------------------
+
+function coinChange(num){
+    let dollar = 100
+    let quarter = 25
+    let dime = 10
+    let nickle = 5
+    let penny = 1
+    let dollarCount, quarterCount, dimeCount, nickleCount, pennyCount
+    if (num > dollar){
+        dollarCount = Math.floor(num/dollar)
+        num -= dollar * dollarCount
     }
-    console.log(arr)
+    if (num > quarter){
+        quarterCount = Math.floor(num/quarter)
+        num -= quarter * quarterCount
+    }
+    if (num > dime){
+        dimeCount = Math.floor(num/dime)
+        num -= dime * dimeCount
+    }
+    if (num > nickle){
+        nickleCount = Math.floor(num/nickle)
+        num -= nickle * nickleCount
+    }
+    if (num > penny){
+        pennyCount = Math.floor(num/penny)
+        num -= penny * pennyCount
+    }
+    console.log('Dollar:' + dollarCount + ', Quarter:' + quarterCount + ', Dime:' + dimeCount + ', Nickle: ' + nickleCount + ', Penny: ' + pennyCount)
 }
 
-bubbleSort([5, 3, 1, 6, 7, 2, 8, 4])
+coinChange(312)
+coinChange(78)
+// coinChange({dollars: 2, dimes: 15, pennies: 5})
