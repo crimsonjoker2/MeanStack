f196563dbb8bce042fd5332246ed14a748c654be
Daniel Long <skillhaven@outlook.com>
2020-05-07 22:20:41 -0500

    Traveling

--- JS OOP/Hundred_acre_Wood.js
+++ JS OOP/Hundred_acre_Wood.js
@@ -1,14 +1,19 @@
-let chris = {character: "Christopher Robin"};
-let kanga = {character: "Kanga"};
-let piglet = {character: "Piglet"};
-let tigger = {character: "Tigger"};
-let pooh = {character: "Whinney the Pooh"};
-let eeyore = {character: "Eeyore"};
-let heffalumps = {character: "Heffalumps"};
-let owl = {character: "Owl"};
-let rabbit = {character: "Rabbit"};
-let gopher = {character: "Gopher"};
-let bees = {character: "Bees"};
+let chris = { character: "Christopher Robin" };
+let kanga = { character: "Kanga" };
+let piglet = { character: "Piglet" };
+let tigger = { character: "Tigger" };
+let pooh = { character: "Whinney the Pooh" };
+let eeyore = { character: "Eeyore" };
+let heffalumps = { character: "Heffalumps" };
+let owl = { character: "Owl" };
+let rabbit = { character: "Rabbit" };
+let gopher = { character: "Gopher" };
+let bees = { character: "Bees" };
+let player = {
+    location: tigger
+
+}
+// console.log(player.location.character)
 
 chris.north = kanga;
 chris.south = pooh;
@@ -44,3 +49,51 @@ eeyore.south = kanga;
 eeyore.east = heffalumps;
 
 heffalumps.west = eeyore;
+
+function move(direction) {
+    if (direction == "north") {
+        temp = player.location
+        player.location = player.location.north
+        if (player.location === undefined) {
+            player.location = temp
+            return console.log("You may not go that way!")
+        }
+
+    }
+    else if (direction == "south") {
+        temp = player.location
+        player.location = player.location.south
+        if (player.location === undefined) {
+            player.location = temp
+            return console.log("You may not go that way!")
+        }
+
+    }
+    else if (direction == "west") {
+        temp = player.location
+        player.location = player.location.west
+        if (player.location === undefined) {
+            player.location = temp
+            return console.log("You may not go that way!")
+        }
+
+    }
+    else {
+        temp = player.location
+        player.location = player.location.east
+        if (player.location === undefined) {
+            player.location = temp
+            return console.log("You may not go that way!")
+        }
+
+    }
+    return console.log('You are now at ' + player.location.character + "'s house.")
+}
+
+
+move('north');
+move('north');
+move('east');
+move('east');
+move('east');
+move('west');
