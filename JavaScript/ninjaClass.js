adc211ae23f774818443d222437cfc0bb10d16fd
Daniel Long <skillhaven@outlook.com>
2020-05-09 21:28:07 -0500

    Ninja Class 3

--- JavaScript/ninjaClass.js
+++ JavaScript/ninjaClass.js
@@ -1,24 +1,72 @@
-function Ninja(name, health = 100, speed=3, strength = 3){
-    var speed = speed;
-    var strength = strength;
+//ES5-----------
+// function Ninja(name, health = 100, speed=3, strength = 3){
+//     var speed = speed;
+//     var strength = strength;
+//     this.name = name;
+//     this.health = health;
+
+//     this.sayName = function(){
+//         console.log("My name is " + this.name + ".");
+//         return this
+//     }
+
+//     this.showStats = function(){
+//         console.log("Name: " + this.name + ", Health: " + this.health + ", Speed: " + speed + ", Strength: " + strength);
+//         return this;
+//     }
+
+//     this.drinkSake = function(){
+//         this.health += 10;
+//         return this;
+//     }
+//     this.punch = function(target){
+//         if (target instanceof Ninja){
+//         target.health -= 5;
+//         console.log(target.name + "was punched by " + this.name + " and lost 5 health.");
+//         return this;}
+//         else{
+//             console.log("You aren't a ninja, you're an imposter!")
+//             return this;
+//         }
+//     }
+//     this.kick = function(target){
+//         if (target instanceof Ninja){
+//         var damage = 15 * strength;
+//         target.health -= damage;
+//         console.log(target.name + " was kicked by " + this.name + " and lost " + damage + " health.");
+//         return this;
+//     }
+//         else{
+//             console.log("You're no ninja, Pathetic.")
+//             return this;
+//         }
+//     }
+// }
+
+
+//ES6-----------
+class Ninja{
+    constructor(name, health = 100, speed=3, strength = 3){
+    this.speed = speed;
+    this.strength = strength;
     this.name = name;
     this.health = health;
-
-    this.sayName = function(){
+    }
+    sayName(){
         console.log("My name is " + this.name + ".");
         return this
     }
 
-    this.showStats = function(){
-        console.log("Name: " + this.name + ", Health: " + this.health + ", Speed: " + speed + ", Strength: " + strength);
+    showStats(){
+        console.log("Name: " + this.name + ", Health: " + this.health + ", Speed: " + this.speed + ", Strength: " + this.strength);
         return this;
     }
 
-    this.drinkSake = function(){
+    drinkSake(){
         this.health += 10;
         return this;
     }
-    this.punch = function(target){
+    punch(target){
         if (target instanceof Ninja){
         target.health -= 5;
         console.log(target.name + "was punched by " + this.name + " and lost 5 health.");
@@ -28,9 +76,9 @@ function Ninja(name, health = 100, speed=3, strength = 3){
             return this;
         }
     }
-    this.kick = function(target){
+    kick(target){
         if (target instanceof Ninja){
-        var damage = 15 * strength;
+        var damage = 15 * this.strength;
         target.health -= damage;
         console.log(target.name + " was kicked by " + this.name + " and lost " + damage + " health.");
         return this;
@@ -41,9 +89,20 @@ function Ninja(name, health = 100, speed=3, strength = 3){
         }
     }
 }
-
+class Sensei extends Ninja{
+    constructor(name, health = 200, speed=10, strength = 10, wisdom = 10){
+        super(name, health, speed, strength);
+        this.wisdom = wisdom
+    }
+    speakWisdom(){
+        super.drinkSake();
+        console.log("What one programmer can do in one month, two programmers can do in two months.");
+        return this;
+    }
+}
 var ninja1 = new Ninja ("Miyamoto");
 var ninja2 = new Ninja ("Jinto");
+var sensei1 = new Sensei("Shintaki");
 var potato = "Potato"
 var carrot = "Carrot"
 ninja1.sayName();
@@ -52,5 +111,8 @@ ninja1.punch(ninja2);
 ninja2.kick(ninja1);
 ninja1.kick(potato);
 ninja2.punch(carrot);
+sensei1.showStats();
+sensei1.speakWisdom();
+sensei1.showStats();
 ninja1.showStats();
 ninja2.showStats();
